e69293f20bbe7d92a2ec3debec87ccbbef81ab61
danielqueirozc <danielqueiroz.job@gmail.com>
2025-11-09 17:33:31 -0300

    feat: create structure frontend with tailwindcss, vite, tanstack router, shiki and resizable-panels

---
+++ web/src/components/section-title.tsx
@@ -0,0 +1,14 @@
+import type { ComponentProps } from "react";
+import { twMerge } from "tailwind-merge";
+
+interface SectionTitle extends ComponentProps<'h3'> {}
+
+export function SectionTitle({className, ...props}: SectionTitle) {
+  return (
+		<h3 
+      className={twMerge('text-base font-semibold text-zinc-100', className)}
+      {...props}
+    />
+
+  )
+}
---
+++ web/src/components/ui/badge.tsx
@@ -0,0 +1,18 @@
+import type { ComponentProps } from 'react'
+import { twMerge } from 'tailwind-merge'
+
+interface BadgeProps extends ComponentProps<'span'> {}
+
+export function Badge({ className, ...props }: BadgeProps) {
+	// desestruturar para poder pegar o className em uma variavel separada para nao sobrescrever os classNames que ja tem
+	return (
+		<span
+			className={twMerge(
+				'px-3 py-1 rounded-lg border font-mono text-sm font-semibold border-zinc-600 bg-zinc-800 text-zinc-100',
+				className,
+			)} /// unir os className
+			{...props}
+		/>
+			// nao precisei colocar children porque o children ja é uma prop
+	)
+}
